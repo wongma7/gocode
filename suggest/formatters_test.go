8a46921806387d5389e863f4b085cc9be536b020
Matthew Dempsky <mdempsky@google.com>
2018-04-27 11:59:33 -0700

    suggest: fix tests

--- suggest/formatters_test.go
+++ suggest/formatters_test.go
@@ -12,40 +12,47 @@ func TestFormatters(t *testing.T) {
 
 	num := len("client")
 	candidates := []suggest.Candidate{{
-		Class: "func",
-		Name:  "client_auto_complete",
-		Type:  "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_auto_complete",
+		Type:    "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)",
 	}, {
-		Class: "func",
-		Name:  "client_close",
-		Type:  "func(cli *rpc.Client, Arg0 int) int",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_close",
+		Type:    "func(cli *rpc.Client, Arg0 int) int",
 	}, {
-		Class: "func",
-		Name:  "client_cursor_type_pkg",
-		Type:  "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string)",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_cursor_type_pkg",
+		Type:    "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string)",
 	}, {
-		Class: "func",
-		Name:  "client_drop_cache",
-		Type:  "func(cli *rpc.Client, Arg0 int) int",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_drop_cache",
+		Type:    "func(cli *rpc.Client, Arg0 int) int",
 	}, {
-		Class: "func",
-		Name:  "client_highlight",
-		Type:  "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int)",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_highlight",
+		Type:    "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int)",
 	}, {
-		Class: "func",
-		Name:  "client_set",
-		Type:  "func(cli *rpc.Client, Arg0, Arg1 string) string",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_set",
+		Type:    "func(cli *rpc.Client, Arg0, Arg1 string) string",
 	}, {
-		Class: "func",
-		Name:  "client_status",
-		Type:  "func(cli *rpc.Client, Arg0 int) string",
+		Class:   "func",
+		PkgPath: "gocode",
+		Name:    "client_status",
+		Type:    "func(cli *rpc.Client, Arg0 int) string",
 	}}
 
 	var tests = [...]struct {
 		name string
 		want string
 	}{
-		{"json", `[6, [{"class": "func", "name": "client_auto_complete", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)"}, {"class": "func", "name": "client_close", "type": "func(cli *rpc.Client, Arg0 int) int"}, {"class": "func", "name": "client_cursor_type_pkg", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string)"}, {"class": "func", "name": "client_drop_cache", "type": "func(cli *rpc.Client, Arg0 int) int"}, {"class": "func", "name": "client_highlight", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int)"}, {"class": "func", "name": "client_set", "type": "func(cli *rpc.Client, Arg0, Arg1 string) string"}, {"class": "func", "name": "client_status", "type": "func(cli *rpc.Client, Arg0 int) string"}]]`},
+		{"json", `[6, [{"class": "func", "name": "client_auto_complete", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)", "package": "gocode"}, {"class": "func", "name": "client_close", "type": "func(cli *rpc.Client, Arg0 int) int", "package": "gocode"}, {"class": "func", "name": "client_cursor_type_pkg", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string)", "package": "gocode"}, {"class": "func", "name": "client_drop_cache", "type": "func(cli *rpc.Client, Arg0 int) int", "package": "gocode"}, {"class": "func", "name": "client_highlight", "type": "func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int)", "package": "gocode"}, {"class": "func", "name": "client_set", "type": "func(cli *rpc.Client, Arg0, Arg1 string) string", "package": "gocode"}, {"class": "func", "name": "client_status", "type": "func(cli *rpc.Client, Arg0 int) string", "package": "gocode"}]]`},
 		{"nice", `Found 7 candidates:
   func client_auto_complete(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)
   func client_close(cli *rpc.Client, Arg0 int) int
@@ -75,13 +82,13 @@ client_set,,func(cli *rpc.Client, Arg0, Arg1 string) string
 client_status,,func(cli *rpc.Client, Arg0 int) string
 `[1:]},
 		{"csv", `
-func,,client_auto_complete,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int)
-func,,client_close,,func(cli *rpc.Client, Arg0 int) int
-func,,client_cursor_type_pkg,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string)
-func,,client_drop_cache,,func(cli *rpc.Client, Arg0 int) int
-func,,client_highlight,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int)
-func,,client_set,,func(cli *rpc.Client, Arg0, Arg1 string) string
-func,,client_status,,func(cli *rpc.Client, Arg0 int) string
+func,,client_auto_complete,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int, Arg3 gocode_env) (c []candidate, d int),,gocode
+func,,client_close,,func(cli *rpc.Client, Arg0 int) int,,gocode
+func,,client_cursor_type_pkg,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 int) (typ, pkg string),,gocode
+func,,client_drop_cache,,func(cli *rpc.Client, Arg0 int) int,,gocode
+func,,client_highlight,,func(cli *rpc.Client, Arg0 []byte, Arg1 string, Arg2 gocode_env) (c []highlight_range, d int),,gocode
+func,,client_set,,func(cli *rpc.Client, Arg0, Arg1 string) string,,gocode
+func,,client_status,,func(cli *rpc.Client, Arg0 int) string,,gocode
 `[1:]},
 	}
 
